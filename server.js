445041dc7ff17e4f5c137d2a2436c9859162b800
Joe Davine <joe@joedavine.com>
2015-06-10 08:13:47 +0100

    added in jquery and started some styles and logic on the server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+/* GET HOMEPAGE */
 app.get('/', function (req, res) {
     res.render('home', {layout: 'main'});
 });
@@ -14,3 +15,12 @@ app.listen(3000);
 
 app.use(express.static('public'));
 
+
+var userChoice = '';
+
+
+if(userChoice === ''){
+    console.log('True');
+} else {
+    console.log('False');
+}
